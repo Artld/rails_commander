ee378f41ac250f2f731b24fff88254d6aedaf6bc
Artld <artld@expressmail.dk>
2018-01-24 17:52:28 +0200

    Implement webhooks controller

---
+++ app/controllers/webhooks_controller.rb
@@ -0,0 +1,40 @@
+require 'thread'
+
+# Parses request from Telegram bot
+class WebhooksController < ApplicationController
+  def callback
+    Thread.new do
+      # sender = TelegramSender.new(user, chat)
+      # OptionParser.new(text, sender)
+    end
+    render nothing: true, head: :ok
+  end
+
+  private
+
+  def message
+    params['message']
+  end
+
+  def text
+    message[:text]
+  end
+
+  def chat
+    message[:chat][:id]
+  end
+
+  def from
+    message[:from]
+  end
+
+  def user
+    User.find_by(telegram_id: from[:id]) || register_user
+  end
+
+  def register_user
+    u = User.find_or_initialize_by(telegram_id: from[:id])
+    u.update_attributes!(username: from[:username], first_name: from[:first_name], last_name: from[:last_name])
+    u
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  post "/webhooks/telegram_#{Rails.application.secrets.webhook_token}" => 'webhooks#callback'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
