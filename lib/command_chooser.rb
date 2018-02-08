e904644f2b1290c74c1efd883a366a354b284953
Artld <artld@expressmail.dk>
2018-02-08 17:49:53 +0200

    Fix commands in lowercase

--- lib/command_chooser.rb
+++ lib/command_chooser.rb
@@ -2,10 +2,9 @@ class CommandChooser
   def start(user, chat, text)
     sender = BotSender.new chat
     args = text.split
-    c = args[0]
-    command = c.start_with?('/') ? c[1, -1] : c
+    command = args[0].gsub('/', '')
     begin
-      the_class = Object.const_get "Command::#{command}"
+      the_class = Object.const_get "Command::#{command.capitalize}"
     rescue NameError
       sender.send_text "unknown command #{command}"
     end
