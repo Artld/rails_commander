a51c6bb350e91bd8301b6919ac3ff4e2cac802fd
Artld <artld@expressmail.dk>
2018-02-08 21:14:46 +0200

    Extend Help to show available commands

--- lib/command/help.rb
+++ lib/command/help.rb
@@ -2,6 +2,14 @@ class Command::Help
   include Command::Common
 
   def perform
-    @sender.send_text 'This is my help'
+    #classes = Command.constants.select { |c| Command.const_get(c).is_a? Class }
+    #commands = classes.map { |c| c.to_s.downcase }.join "\n"
+
+    files = Dir['lib/command/*.rb']
+    commands = files.map { |f| f.split('/').last.gsub('.rb', '') }.join "\n"
+
+    @sender.send_text "Usage: COMMAND [OPTIONS]\n"\
+                      "Available commands:\n"\
+                      "#{commands}"
   end
 end
