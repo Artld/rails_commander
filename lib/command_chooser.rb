a57dea04e24db65dd819ad4590d6569ee7b40e0f
Artld <artld@expressmail.dk>
2018-02-08 12:58:39 +0200

    Fix commands starting with /

--- lib/command_chooser.rb
+++ lib/command_chooser.rb
@@ -2,10 +2,11 @@ class CommandChooser
   def start(user, chat, text)
     sender = BotSender.new chat
     args = text.split
-    command = args[0]
+    c = args[0]
+    command = c.start_with?('/') ? c[1, -1] : c
     begin
       the_class = Command.const_get command
-    rescue NameError => e
+    rescue NameError
       sender.send_text "unknown command #{command}"
     end
     the_class.new(user, sender).parse(args[1, -1])
